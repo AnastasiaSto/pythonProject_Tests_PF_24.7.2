36f08dff9a263721500e713a251b3e738b038526
Anastasia <anastasija100@yandex.ru>
2023-05-24 22:54:11 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+valid_email = "n.stovbyra@gmail.com"
+valid_password = "010808"
+empty_email = ""
+empty_password = ""
+empty_auth_key = {"key": ""}
+incorrect_auth_key = {"key": "000000000000000000000000000000000000000000000"}
